5b79d2bd782a5e54f219c0eac797cb27990384f8
jared <jaredrotich3@gmail.com>
2025-06-21 23:56:21 +0300

    added logic in app

--- server/app.py
+++ server/app.py
@@ -14,11 +14,13 @@ def show_session(key):
     response = make_response(jsonify({
         'session': {
             'session_key': key,
-            'session_value': session[key],
+            'session_value': session.get(key),
             'session_accessed': session.accessed,
         },
-        'cookies': [{cookie: request.cookies[cookie]}
-            for cookie in request.cookies],
+        'cookies': [
+            {cookie: request.cookies[cookie]}
+            for cookie in request.cookies
+        ],
     }), 200)
 
     response.set_cookie('mouse', 'Cookie')
